fa8076a6444d75ab730832247078297258bf40e2
Joey Cardosi <joey@Joeys-MacBook-Air.local>
2018-03-22 18:45:38 -0500

    okay, NOW I finished section 2. :D

--- Lessons/my-first-app/src/app/servers/servers.component.ts
+++ Lessons/my-first-app/src/app/servers/servers.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [`
+    .white-text {
+      color: white;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 	allowNewServer = false;
 	serverCreationStatus = "No server was created";
 	serverName = '';
 	userName = '';
+	serverCreated = false;
+	servers = ['testServer', 'testServer 2'];
+
+  clicks = [];
+	showPassword = false;
 
   constructor() {
   	setTimeout(() => {
@@ -21,7 +31,19 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
   	this.serverCreationStatus = 'Server was created~! Name is ' + this.serverName;
+  }
+
+  togglePassword() {
+    this.showPassword = !(this.showPassword);
+	  this.clicks.push(new Date());
+  }
+
+  getColor() {
+    console.log("clicks length: " + (this.clicks.length >= 5));
+    return this.clicks.length >= 5 ? 'blue' : 'none';
   }
 
   onUpdateServerName(event: Event) {
